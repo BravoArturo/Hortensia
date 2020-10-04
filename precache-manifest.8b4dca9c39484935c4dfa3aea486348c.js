3aab3bac41fd83629133b7a57b33f58e00a74a86
Arturo Bravo <aarturoo97@gmail.com>
2020-10-04 10:45:59 -0300

    Updates

---
+++ precache-manifest.8b4dca9c39484935c4dfa3aea486348c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724959d8ce8025d5ee3d194add374942",
+    "url": "/Hortensia/index.html"
+  },
+  {
+    "revision": "ed5be05cafb1e64f1bd0",
+    "url": "/Hortensia/static/css/main.e16e0a37.chunk.css"
+  },
+  {
+    "revision": "a44a933aa514b3c7147c",
+    "url": "/Hortensia/static/js/2.85b19f98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Hortensia/static/js/2.85b19f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed5be05cafb1e64f1bd0",
+    "url": "/Hortensia/static/js/main.6e5c9060.chunk.js"
+  },
+  {
+    "revision": "0b12b0816c9a5b664b5e",
+    "url": "/Hortensia/static/js/runtime-main.700f231d.js"
+  },
+  {
+    "revision": "9c071c289df6a77d050a3fddd07cb66c",
+    "url": "/Hortensia/static/media/difusorgral.9c071c28.jpg"
+  },
+  {
+    "revision": "cbef198ce1bae97633cf273f9da1dd1d",
+    "url": "/Hortensia/static/media/floreschicasgral.cbef198c.jpg"
+  },
+  {
+    "revision": "5d704a1a0e35e9599b9058497ea435d9",
+    "url": "/Hortensia/static/media/floresgrandesgral.5d704a1a.jpg"
+  },
+  {
+    "revision": "df1d37db88727405c2973860e4f8a762",
+    "url": "/Hortensia/static/media/homespraygral.df1d37db.jpg"
+  },
+  {
+    "revision": "caf3076398724e1f61a7df62312d5e91",
+    "url": "/Hortensia/static/media/logoh.caf30763.jpg"
+  },
+  {
+    "revision": "479e3b7a377fdc58647ff64d09d04b4f",
+    "url": "/Hortensia/static/media/velascementogral.479e3b7a.jpg"
+  },
+  {
+    "revision": "2cfe2b78cc91095a8e2bffe4c6f7639a",
+    "url": "/Hortensia/static/media/velasvidriogral.2cfe2b78.jpg"
+  }
+]);
